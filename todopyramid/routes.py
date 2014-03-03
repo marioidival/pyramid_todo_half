6c716f772897bb3044e7d67244bbb55d3c16f5b7
Mario Idival <marioidival@gmail.com>
2014-03-03 11:59:08 -0300

    add routes new, edit, delete

--- todopyramid/routes.py
+++ todopyramid/routes.py
@@ -1,5 +1,8 @@
 def includeme(config):
 
     config.add_route('index', '/')
+    config.add_route('new', '/new')
+    config.add_route('edit', '/edit/{todo_id}')
+    config.add_route('delete', '/delete/{todo_id}')
 
     config.scan('todopyramid.views')
