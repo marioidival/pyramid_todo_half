94c505b0413ed418b2f86bf01a14c6c77518db5d
Mario Idival <marioidival@gmail.com>
2014-04-22 01:12:04 -0300

    add some tests

--- todopyramid/tests.py
+++ todopyramid/tests.py
@@ -1,6 +1,7 @@
 import unittest
 from pyramid import testing
 from pyramid.i18n import TranslationStringFactory
+from pymongo import MongoClient
 
 _ = TranslationStringFactory('todopyramid')
 
@@ -8,13 +9,20 @@ _ = TranslationStringFactory('todopyramid')
 class ViewTests(unittest.TestCase):
 
     def setUp(self):
-        testing.setUp()
+        self.request = testing.DummyRequest()
+        self.db = MongoClient().todopyramid
+        self.request.db = self.db
+        testing.setUp(request=self.request)
 
     def tearDown(self):
         testing.tearDown()
 
-    def test_my_view(self):
+    def test_my_index(self):
         from todopyramid.views import index
-        request = testing.DummyRequest()
-        response = index(request)
-        self.assertEqual(response['project'], 'todopyramid')
+        response = index(self.request)
+        self.assertEqual(response['todolist'], list(self.db.todolist.find()))
+
+    def test_my_new_todo(self):
+        from todopyramid.views import new
+        resp = new(self.request)
+        self.assertEqual(resp['todo'], {})
