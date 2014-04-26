8834cc69fda5fe3d09cd477db24201330b48a360
Mario Idival <marioidival@gmail.com>
2014-04-26 00:16:13 -0300

    remove requirements in setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires=[
-    'pyramid>=1.0.2',
-    'pyramid_jinja2',
-    'pymongo==2.5',
 ]
 
 setup(name='todopyramid',
